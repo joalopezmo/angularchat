c6c75227cfda2110bc73a2c7fe0031ce5941de27
joalopezmo <joalopezmo@uoc.edu>
2024-03-03 23:33:10 +0100

    Add text message box component

--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -3,4 +3,7 @@ export * from './chat-bubbles/myMessage/myMessage.component';
 
 export * from './sidebarMenuItem/sidebarMenuItem.component';
 
+export * from './text-boxes/textMessageBox/textMessageBox.component';
+
 export * from './typingLoader/typingLoader.component';
+
--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GptMessagesComponent, MyMessageComponent, TypingLoaderComponent } from '@components/index';
+import { GptMessagesComponent, MyMessageComponent, TextMessageBoxComponent, TypingLoaderComponent } from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
-  imports: [CommonModule, GptMessagesComponent, MyMessageComponent, TypingLoaderComponent],
+  imports: [CommonModule, GptMessagesComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
