db528a61462f4378788f2d98c1aca10fa2014db0
joalopezmo <joalopezmo@uoc.edu>
2024-03-03 23:45:41 +0100

    Update textMessageBox.component.html and orthographyPage.component.html

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -9,4 +9,10 @@ import { GptMessagesComponent, MyMessageComponent, TextMessageBoxComponent, Typi
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent {}
+export default class OrthographyPageComponent {
+
+  handleMessage(prompt: string) {
+    console.log(prompt);
+  }
+
+}
