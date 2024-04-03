de2ab6c65c5bdc1e8945cbc49f63cf9f137e8645
joalopezmo <joalopezmo@uoc.edu>
2024-04-03 23:43:53 +0200

    Agregados componentes de caja de texto con archivo y mensajes en la página de ortografía

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,18 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GptMessagesComponent, MyMessageComponent, TextMessageBoxComponent, TypingLoaderComponent } from '@components/index';
+import {
+  GptMessagesComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+  TextMessageBoxFileComponent,
+  TextMessageEvent,
+  TypingLoaderComponent,
+} from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
-  imports: [CommonModule, GptMessagesComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent],
+  imports: [
+    CommonModule,
+    GptMessagesComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+    TextMessageBoxFileComponent,
+  ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrthographyPageComponent {
-
   handleMessage(prompt: string) {
     console.log(prompt);
   }
-
+  handleMessageWithFile(event: TextMessageEvent) {
+    console.log({ prompt });
+  }
 }
